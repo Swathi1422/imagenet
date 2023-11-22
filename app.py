f719e55c9159b658e6a63e4b411384bfd3558c00
KIRANKALLA <kallakiran1974@gmail.com>
2023-11-22 12:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from PIL import Image
 import numpy as np
-from keras.models import load_model
+#from keras.models import load_model
+import pickle
 import cv2
 from keras.applications.inception_v3 import preprocess_input,decode_predictions
 import streamlit as st
 
-clf = load_model('inception')
+#clf = load_model('inception')
+pickle_in = open('mobilenet.pkl','rb')
+clf = pickle.load(pickle_in)
 img = st.file_uploader('Take any picture')
 if img:
     image = Image.open(img)
